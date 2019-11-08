4b20b94c3868d5c144277e22c5bb1a3e4a7f8b73
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-08 11:34:24 -0500

    reducer logic for fetch and post

--- smurfs/src/reducers/reducers.js
+++ smurfs/src/reducers/reducers.js
@@ -10,5 +10,45 @@ const initialState = {
 };
 
 export const reducer = (state = initialState, action) => {
-	return state;
+	switch (action.type) {
+		case "GETTING_SMURFS":
+			return {
+				...state,
+				gettingSmurf: true
+			};
+		case "GOT_SMURFS":
+			return {
+				...state,
+				smurfs: action.payload,
+				gettingSmurf: false
+			};
+		case "FAILED_SMURFS":
+			return {
+				...state,
+				error: action.payload,
+				gettingSmurf: false
+			};
+		case "POSTING": {
+			return {
+				...state,
+				postingSmurf: true
+			};
+		}
+		case "POSTED": {
+			return {
+				...state,
+				smurfs: action.payload,
+				postingSmurf: false
+			};
+		}
+		case "POST_FAILED": {
+			return {
+				...state,
+				postingSmurf: false,
+				error: action.payload
+			};
+		}
+		default:
+			return state;
+	}
 };
