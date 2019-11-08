d64317acdceae2ad09868321f7888ee2a2ddf85a
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-08 11:42:04 -0500

    action for getting api data created and App refactored into functional component

---
+++ smurfs/src/actions/actions.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const getSmurfs = () => {
+	return function(dispatch) {
+		dispatch({ type: "LOADING_GET" });
+
+		return axios
+			.get(`http://localhost:3333/smurfs`)
+			.then(res => {
+				// dispatch({ type: "GOT_SMURFS", payload: res.data });
+				console.log(res);
+			})
+			.catch(err => dispatch({ type: "FAILED_SMURFS", payload: err }));
+	};
+};
