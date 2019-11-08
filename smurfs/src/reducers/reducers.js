0cbf17cbf486e242578af937b5efa3e171359f37
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-08 11:13:06 -0500

    initialState object created and empty reducer exported

---
+++ smurfs/src/reducers/reducers.js
@@ -0,0 +1,14 @@
+const initialState = {
+	smurfs: [],
+	gettingSmurf: false,
+	postingSmurf: false,
+
+	error: "",
+	name: "",
+	age: "",
+	height: ""
+};
+
+export const reducer = (state = initialState, action) => {
+	return state;
+};
